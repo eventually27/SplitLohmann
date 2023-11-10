1d685925eaae963c072447337753ec2495b181df
JIN SU LEE <46187356+eventually27@users.noreply.github.com>
2023-11-10 15:56:20 +0900

    Update CBP_dat.py

--- CBP_dat.py
+++ CBP_dat.py
@@ -31,11 +31,41 @@ r_mat = 1.456
 
 
 phase_modulation = np.exp(-1j*2*np.pi*(h1 + h2)*(r_mat - r_air)/wav)
+phase2sag = np.angle(phase_modulation)*wav/(2*np.pi) * 1e+3
 
-plt.imshow(np.angle(phase_modulation),cmap='gray')
-
+plt.imshow(phase2sag,cmap='gray')
 plt.show()
+
 phase_modulation = np.fft.ifftshift(np.fft.ifft2(phase_modulation))
 
 plt.imshow(np.abs(phase_modulation))
 plt.show()
+
+def save_phase2sag_to_file(data):
+    output = ""
+    output += "3333 3333 0.036 0.036 0 0 0\n"
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            output += "%f 0 0 0 0\n" % data[i, j]
+    return output
+
+phase2sag_text = save_phase2sag_to_file(phase2sag)
+
+with open("grid_data.dat", "w") as file:
+    file.write(phase2sag_text)
+
+# #dat 파일 생성 phase to sag"
+# data = """10 10 0.036 0.036 0 0 0
+# 1 0 0 0 0
+# 0 0 0 0 0
+# 1 0 0 0 0
+# 1 0 0 0 0
+# 0 0 0 0 0
+# 1 0 0 0 0
+# 1 0 0 0 0
+# 1 0 0 0 0
+# 0 0 0 0 0
+# 1 0 0 0 0"""
+
+# with open("data.dat", "w") as file:
+#     file.write(data)
